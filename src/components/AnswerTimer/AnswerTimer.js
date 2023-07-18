0dba25e70478863c0bafcd3317f8c46d730099c2
IflaMushtaq <nahamiiflamushtaq@gmail.com>
2023-07-18 16:12:31 +0530

    UI Part completed

--- src/components/AnswerTimer/AnswerTimer.js
+++ src/components/AnswerTimer/AnswerTimer.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import './AnswerTimer.css'
+import { useEffect, useRef, useState } from 'react';
 
-function AnswerTimer() {
+function AnswerTimer({duration, onTimeUp}) {
+  const [counter, setCounter]=useState(0);
+  const [progressLoader, setProgressLoader]=useState(0);
+  const intervalRef=useRef();
+
+  useEffect(()=>{
+    intervalRef.current=setInterval(()=>{
+      setCounter((cur)=>cur+1);
+    },1000);
+
+    return ()=>clearInterval(intervalRef.current)
+  },[]);
+
+  useEffect(()=>{
+    setProgressLoader(100*(counter/duration));
+    if(counter===duration){
+      clearInterval(intervalRef.current);
+
+      setTimeout(() => {
+        onTimeUp();
+      }, 1000);
+    }
+  },[counter]);
+
   return (
     <div className='timer-line'>
-      <div className='progress'>
-        
+      <div 
+      style={{
+        width:`${progressLoader}%`,  
+      }} 
+      className='progress'>
       </div>
     </div>
   )
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,10 +2,31 @@ import React from 'react';
 import { useState } from 'react';
 import { resultInitialState } from '../questions/Constants';
 import AnswerTimer from '../AnswerTimer/AnswerTimer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRefresh } from '@fortawesome/free-solid-svg-icons';
+import { bounce, slideInLeft } from "react-animations";
+import Radium, { StyleRoot } from "radium";
 import './QuizStyle.css';
 
 
 const Quiz=({questions})=> {
+
+  const styles = {
+    slideInLeft: {
+      animation: 'x 1.5s',
+      animationName: Radium.keyframes(slideInLeft, 'slideInLeft'), 
+    },
+    bounce: {
+      animation: "x 1s",
+      animationName: Radium.keyframes(bounce, "bounce"),
+    },
+  };
+
+  const stylesHead = {
+    fontSize: "3rem",
+    ...styles.slideInDown,
+  };
+
   const [currentQuestion, setCurrentQuestion]=useState(0);
   const {question, choices, correctAnswer}=questions[currentQuestion];
 
@@ -23,10 +44,10 @@ const Quiz=({questions})=> {
     }
   }
 
-  const onClickNext=()=>{
+  const onClickNext=(finalAnswer)=>{
     setAnswerIndex(null);
     setResult((prev)=>
-      answer?{
+    finalAnswer?{
         ...prev,
         score:prev.score+5,
         correctAnswers:prev.correctAnswers+1
@@ -34,7 +55,11 @@ const Quiz=({questions})=> {
         ...prev,
         wrongAnswers:prev.wrongAnswers+1
       }
-    )
+    );
+
+    if (!finalAnswer) {
+      console.log("Incorrect");
+    }
 
     if(currentQuestion!==questions.length-1){
       setCurrentQuestion((prev)=>prev+1);
@@ -49,60 +74,69 @@ const Quiz=({questions})=> {
     setShowResult(false)
   }
 
+  const handleTimeUp=()=>{
+    setAnswer(false)
+    onClickNext(false)
+  };
+
   return (
     <div className='quiz-container'>
-      <h1 className='quiz-title' style={{fontSize:"3rem"}}>QuranIQ</h1>
-      <div className='quiz-box mb-3'>
-      
-      {!showResult?(
-        <>
-        <AnswerTimer/>
-        <div className='text-warning text-weight-bold'>
-          <span>
-            {currentQuestion+1}
-          </span>
-          <span>
-            /{questions.length}
-          </span>
+      <StyleRoot>
+        <div style={styles.slideInLeft}>
+        <h1 className='quiz-title'>QuranIQ</h1>
+          <div className='quiz-box mb-3'>
+            {!showResult ? (
+              <>
+                <div className='text-warning text-weight-bold mb-3' style={{borderTopLeftRadius: '5px', borderTopRightRadius:'5px'}}>
+                <AnswerTimer duration={10} onTimeUp={handleTimeUp} />
+                  <span>
+                    {currentQuestion + 1}
+                  </span>
+                  <span>
+                    /{questions.length}
+                  </span>
+                </div>
+                <h4 className='text-white'>{question}</h4>
+                <ul className='p-0'>
+                  {
+                    choices.map((answer, index) => (
+                      <li
+                        onClick={() => onAnswerClick(answer, index)}
+                        key={answer}
+                        className={answerIndex === index ? 'selected-answer' : null}
+                      >
+                        {answer}
+                      </li>
+                    ))
+                  }
+                </ul>
+              </>
+            ) : (
+              <div className='text-center mt-5 text-light quiz-p'>
+                <h2 >Score: &nbsp;<span className='text-warning'>{result.score}</span></h2>
+                <hr className='mb-5' />
+                <p>
+                  Total Questions: &nbsp;<span className='text-warning'>{questions.length}</span>
+                </p>
+                <p>
+                  Correct Answers: &nbsp;<span className='text-warning'>{result.correctAnswers}</span>
+                </p>
+                <p>
+                  Wrong Answers: &nbsp;<span className='text-warning'>{result.wrongAnswers}</span>
+                </p>
+                <button className='btn btn-warning mt-5' style={{fontFamily: "Georgia" }} onClick={tryAgain}>Try Again &nbsp;  <FontAwesomeIcon icon={faRefresh} /></button>
+              </div>
+            )}
+            <div className='quiz-button-container'>
+              {!showResult && (
+                <button className='btn btn-warning' style={{fontFamily: "Georgia" }} onClick={() => onClickNext(answer)} disabled={answerIndex === null}>
+                  {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-        <h4 className='text-white'>{question}</h4>
-        <ul className='p-0'>
-          {
-            choices.map((answer, index)=>(
-              <li
-                onClick={()=>onAnswerClick(answer, index)}
-                key={answer}
-                className={answerIndex===index?'selected-answer':null}
-              >
-                {answer}
-              </li>
-            ))
-          }
-        </ul>
-        <footer>
-          <button onClick={onClickNext} disabled={answerIndex===null}>
-            {currentQuestion===questions.length-1?"Finish":"Next"}
-          </button>
-        </footer>
-        </>
-      ):<div>
-        <h3>Result</h3>
-        <p>
-          Total Questions:<span>{questions.length}</span>
-        </p>
-        <p>
-          Total Score: <span>{result.score}</span>
-        </p>
-        <p>
-          Correct Answers: <span>{result.correctAnswers}</span>
-        </p>
-        <p>
-          Wrong Answers: <span>{result.wrongAnswers}</span>
-        </p>
-        <button onClick={tryAgain}>Try Again</button>
-      </div>}
-      
-      </div>
+      </StyleRoot>
     </div>
   )
 }
